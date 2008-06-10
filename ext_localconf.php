e5adebc85e744cbc9e637f02dbaa9ea22e4dfbdb
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-06-10 14:38:20 +0000

    Cleaned up stuff from the EM Removed unused stuff
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/displaycontroller/trunk@10015 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,8 +3,8 @@ if (!defined ('TYPO3_MODE')) {
  	die ('Access denied.');
 }
 
+// Register plug-ins with standard template
+
 t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_displaycontroller_pi1.php','_pi1','CType',1);
-
-
 t3lib_extMgm::addPItoST43($_EXTKEY,'pi2/class.tx_displaycontroller_pi2.php','_pi2','CType',0);
 ?>
--- ext_tables.php
+++ ext_tables.php
@@ -3,28 +3,16 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+// Register plug-ins (pi1 is cached, pi2 is not cached
+
 t3lib_div::loadTCA('tt_content');
 $TCA['tt_content']['types'][$_EXTKEY.'_pi1']['showitem']='CType;;4;button;1-1-1, header;;3;;2-2-2';
-
-
 t3lib_extMgm::addPlugin(array('LLL:EXT:displaycontroller/locallang_db.xml:tt_content.CType_pi1', $_EXTKEY.'_pi1'),'CType');
 
-
-t3lib_div::loadTCA('tt_content');
 $TCA['tt_content']['types'][$_EXTKEY.'_pi2']['showitem']='CType;;4;button;1-1-1, header;;3;;2-2-2';
-
-
 t3lib_extMgm::addPlugin(array('LLL:EXT:displaycontroller/locallang_db.xml:tt_content.CType_pi2', $_EXTKEY.'_pi2'),'CType');
 
-
-if (TYPO3_MODE == 'BE')	{
-	include_once(t3lib_extMgm::extPath('displaycontroller').'class.tx_displaycontroller_tt_content_tx_displaycontroller_model.php');
-}
-
-
-if (TYPO3_MODE == 'BE')	{
-	include_once(t3lib_extMgm::extPath('displaycontroller').'class.tx_displaycontroller_tt_content_tx_displaycontroller_view.php');
-}
+// Add new columns to tt_content
 
 $tempColumns = array (
 	'tx_displaycontroller_model' => array (		
@@ -35,7 +23,6 @@ $tempColumns = array (
 			'items' => array (
 				array('LLL:EXT:displaycontroller/locallang_db.xml:tt_content.tx_displaycontroller_model.I.0', '0'),
 			),
-			'itemsProcFunc' => 'tx_displaycontroller_tt_content_tx_displaycontroller_model->main',	
 			'size' => 1,	
 			'maxitems' => 1,
 		)
@@ -48,7 +35,6 @@ $tempColumns = array (
 			'items' => array (
 				array('LLL:EXT:displaycontroller/locallang_db.xml:tt_content.tx_displaycontroller_view.I.0', '0'),
 			),
-			'itemsProcFunc' => 'tx_displaycontroller_tt_content_tx_displaycontroller_view->main',	
 			'size' => 1,	
 			'maxitems' => 1,
 		)
@@ -79,8 +65,6 @@ $tempColumns = array (
 	),
 );
 
-
-t3lib_div::loadTCA('tt_content');
 t3lib_extMgm::addTCAcolumns('tt_content',$tempColumns,1);
 t3lib_extMgm::addToAllTCAtypes('tt_content','tx_displaycontroller_model;;;;1-1-1, tx_displaycontroller_view, tx_displaycontroller_dataquery, tx_displaycontroller_focus');
 ?>
