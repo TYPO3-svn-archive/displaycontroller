9fee3c064f4f8101a2d44cac258322df878fbef5
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-07-28 12:48:52 +0000

    Added controller services for each plugin
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/displaycontroller/trunk@10775 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ class.tx_displaycontroller_service.php
@@ -0,0 +1,80 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2008 Francois Suter (Cobweb) <typo3@cobweb.ch>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ * Hint: use extdeveval to insert/update function index above.
+ */
+
+require_once(t3lib_extMgm::extPath('basecontroller', 'services/class.tx_basecontroller_base.php'));
+
+/**
+ * Service for the 'displaycontroller' extension.
+ *
+ * @author	Francois Suter (Cobweb) <typo3@cobweb.ch>
+ * @package	TYPO3
+ * @subpackage	tx_displaycontroller
+ */
+class tx_displaycontroller_service extends tx_basecontroller_base {
+	protected $uid;
+
+	/**
+     * This method reads the information related to the controller from the database
+     * 
+     * @param	integer	$id: primary key of the controller instance
+     */
+	public function loadControllerData($id) {
+		$this->uid = $id;
+		// At this point in time, load all the provider data from the database is not necessary
+    }
+
+	/**
+     * This method is expected to return the primary provider related to the given display controller instance
+     * 
+     * @return	dataprovider	Reference to an object implementing the DataProvider interface
+     */
+	public function getPrimaryProvider() {
+		t3lib_div::loadTCA('tt_content');
+			// Get table where the relation to the provider is stored
+		$mmTable = $GLOBALS['TCA']['tt_content']['columns']['tx_displaycontroller_provider']['config']['MM'];
+		$rows = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('*', $mmTable, "uid_local = '".$this->uid."'");
+		$numRows = count($rows);
+		if ($numRows == 0) {
+			throw new Exception('No provider found');
+        }
+		else {
+			$provider = t3lib_div::makeInstanceService('dataprovider', $rows[0]['tablenames']);
+			$provider->loadProviderData($rows[0]['uid_foreign']);
+			return $provider;
+        }
+    }
+}
+
+
+   
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/displaycontroller/class.tx_displaycontroller_service.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/displaycontroller/class.tx_displaycontroller_service.php']);
+}
+
+?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -12,4 +12,48 @@ t3lib_extMgm::addPItoST43($_EXTKEY, 'pi2/class.tx_displaycontroller_pi2.php', '_
 
 if (!isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['providers'])) $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['providers'] = array();
 if (!isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['consumers'])) $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['consumers'] = array();
+
+// Register Controller services for both plug-ins
+// NOTE 1: the type of service is "datacontroller" and not "controller" to avoid conflict with a possible, future, core "controller" service
+// NOTE 2: the subtype corresponds to the CType
+// NOTE 3: the actual class used is the same for both plug-ins (since both plug-ins are the same, except for the cache)
+
+t3lib_extMgm::addService($_EXTKEY,  'datacontroller' /* sv type */,  'tx_displaycontroller_pi1' /* sv key */,
+		array(
+
+			'title' => 'Display Controller (cached)',
+			'description' => 'Controller service for the (cached) display controller',
+
+			'subtype' => 'displaycontroller_pi1',
+
+			'available' => TRUE,
+			'priority' => 50,
+			'quality' => 50,
+
+			'os' => '',
+			'exec' => '',
+
+			'classFile' => t3lib_extMgm::extPath($_EXTKEY, 'class.tx_displaycontroller_service.php'),
+			'className' => 'tx_displaycontroller_service',
+		)
+	);
+t3lib_extMgm::addService($_EXTKEY,  'datacontroller' /* sv type */,  'tx_displaycontroller_pi2' /* sv key */,
+		array(
+
+			'title' => 'Display Controller (not cached)',
+			'description' => 'Controller service for the (not cached) display controller',
+
+			'subtype' => 'displaycontroller_pi2',
+
+			'available' => TRUE,
+			'priority' => 50,
+			'quality' => 50,
+
+			'os' => '',
+			'exec' => '',
+
+			'classFile' => t3lib_extMgm::extPath($_EXTKEY, 'class.tx_displaycontroller_service.php'),
+			'className' => 'tx_displaycontroller_service',
+		)
+	);
 ?>
