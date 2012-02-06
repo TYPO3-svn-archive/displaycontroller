ed069c81eccf1016e185bf113eae5150e0be54dd
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-02-06 08:35:42 +0000

    Released to TER as version 1.2.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/displaycontroller/trunk@57371 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "displaycontroller".
 #
-# Auto generated 16-08-2011 09:00
+# Auto generated 06-02-2012 09:09
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.1.0',
+	'version' => '1.2.0',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:24:{s:9:"ChangeLog";s:4:"0986";s:10:"README.txt";s:4:"b948";s:30:"class.tx_displaycontroller.php";s:4:"39d3";s:38:"class.tx_displaycontroller_realurl.php";s:4:"0d14";s:38:"class.tx_displaycontroller_service.php";s:4:"8f8e";s:16:"ext_autoload.php";s:4:"a0bf";s:21:"ext_conf_template.txt";s:4:"38b1";s:12:"ext_icon.gif";s:4:"f02f";s:17:"ext_localconf.php";s:4:"8cc2";s:14:"ext_tables.php";s:4:"122e";s:14:"ext_tables.sql";s:4:"62a7";s:16:"ext_typeicon.gif";s:4:"0c33";s:13:"locallang.xml";s:4:"d932";s:27:"locallang_csh_ttcontent.xml";s:4:"a95d";s:16:"locallang_db.xml";s:4:"bc5c";s:15:"wizard_icon.gif";s:4:"b025";s:14:"doc/manual.pdf";s:4:"e840";s:14:"doc/manual.sxw";s:4:"50b2";s:14:"doc/manual.txt";s:4:"c8d7";s:38:"pi1/class.tx_displaycontroller_pi1.php";s:4:"3994";s:46:"pi1/class.tx_displaycontroller_pi1_wizicon.php";s:4:"16b3";s:38:"pi2/class.tx_displaycontroller_pi2.php";s:4:"9ddb";s:46:"pi2/class.tx_displaycontroller_pi2_wizicon.php";s:4:"153e";s:16:"static/setup.txt";s:4:"b445";}',
+	'_md5_values_when_last_written' => 'a:30:{s:9:"ChangeLog";s:4:"7da3";s:10:"README.txt";s:4:"b948";s:30:"class.tx_displaycontroller.php";s:4:"f0d3";s:39:"class.tx_displaycontroller_debugger.php";s:4:"38b7";s:38:"class.tx_displaycontroller_realurl.php";s:4:"0d14";s:38:"class.tx_displaycontroller_service.php";s:4:"8f8e";s:16:"ext_autoload.php";s:4:"58ed";s:21:"ext_conf_template.txt";s:4:"d6a8";s:12:"ext_icon.gif";s:4:"f02f";s:17:"ext_localconf.php";s:4:"8cc2";s:14:"ext_tables.php";s:4:"122e";s:14:"ext_tables.sql";s:4:"62a7";s:16:"ext_typeicon.gif";s:4:"0c33";s:13:"locallang.xml";s:4:"83dd";s:27:"locallang_csh_ttcontent.xml";s:4:"a95d";s:16:"locallang_db.xml";s:4:"bc5c";s:15:"wizard_icon.gif";s:4:"b025";s:14:"doc/manual.pdf";s:4:"a440";s:14:"doc/manual.sxw";s:4:"3700";s:14:"doc/manual.txt";s:4:"fb32";s:23:"lib/kint/Kint.class.php";s:4:"dcea";s:27:"lib/kint/config.default.php";s:4:"ae73";s:22:"lib/kint/view/kint.css";s:4:"899b";s:21:"lib/kint/view/kint.js";s:4:"f06a";s:25:"lib/kint/view/trace.phtml";s:4:"facf";s:38:"pi1/class.tx_displaycontroller_pi1.php";s:4:"3994";s:46:"pi1/class.tx_displaycontroller_pi1_wizicon.php";s:4:"16b3";s:38:"pi2/class.tx_displaycontroller_pi2.php";s:4:"9ddb";s:46:"pi2/class.tx_displaycontroller_pi2_wizicon.php";s:4:"153e";s:16:"static/setup.txt";s:4:"b445";}',
 );
 
 ?>
