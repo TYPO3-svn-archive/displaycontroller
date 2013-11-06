826bca6799d323fdef6f5f972225b491bca6e9b2
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-11-06 16:11:03 +0000

    Removed useless require_once() call, resolves #53387
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/displaycontroller/trunk@81280 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_displaycontroller.php
+++ class.tx_displaycontroller.php
@@ -22,8 +22,6 @@
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 
-require_once(t3lib_extMgm::extPath('tesseract', 'base/class.tx_tesseract_picontrollerbase.php'));
-
 /**
  * Plugin 'Display Controller (cached)' for the 'displaycontroller' extension.
  *
